55bd02cffae8f60909330c23b03e1ef0107cda86
Weder Videira Borges <wederborgesemp@gmail.com>
2025-10-11 18:42:04 -0400

    Pagina Login e formulários criados

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import time
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
@@ -13,6 +13,14 @@ def users():
     lista_usuarios = ["Nicolau BOBO CHERA CHERA"]
 
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login(): 
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True) #Nunca usar isso em prod
