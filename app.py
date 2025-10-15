befab6f75e5b004ce81dcbf1149a5c28b5bf08b4
Wede _Borges <wederborgesemp@gmail.com>
2025-10-15 13:39:10 -0400

    Trabalhando em  alertas nos formularios

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import time
 from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import Form_Criar_Conta, Form_Login
+import email
 
 app = Flask(__name__)
 
@@ -26,15 +27,13 @@ def login():
     form_criar_conta = Form_Criar_Conta()
 
     if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
-        flash(f"Login feito com sucesso no email: {form_login.email_login.data} !", 'sucess')
-        print(request.form)
-        return redirect(url_for('contato'))
-        #Login com sucesso
+        return(redirect(url_for('home')))
+    
     if form_criar_conta.validate_on_submit() and 'botao_submit_criar_conta' in request.form:
-        flash(f"Cadastro realizado com sucesso para o email {form_criar_conta.email_cadastro.data}!", 'primary')
-        return redirect(url_for('contato'))
-        #Cadastro com sucesso   
+        return(redirect(url_for('home')))
+    
     return render_template('login.html', form_login=form_login, form_criar_conta=form_criar_conta)
+    
 
 if __name__ == "__main__":
     app.run(debug=True) #Nunca usar isso em prod
