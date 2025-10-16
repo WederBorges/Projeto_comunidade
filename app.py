e8bbe9b861ec204af83a16571d4f8e62c524b79a
Wede _Borges <wederborgesemp@gmail.com>
2025-10-16 13:31:21 -0400

    Finalizando validadores

--- app.py
+++ app.py
@@ -27,9 +27,11 @@ def login():
     form_criar_conta = Form_Criar_Conta()
 
     if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        flash("Login realizado com sucesso", "sucess")
         return(redirect(url_for('home')))
     
     if form_criar_conta.validate_on_submit() and 'botao_submit_criar_conta' in request.form:
+        flash("Cadastro realizado com sucesso", "Primary")
         return(redirect(url_for('home')))
     
     return render_template('login.html', form_login=form_login, form_criar_conta=form_criar_conta)
