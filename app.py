e0c73d447450a24e4ef3d56a8de21db17fb584ae
Wede _Borges <wederborgesemp@gmail.com>
2025-10-17 13:49:19 -0400

    Criando classes e iniciando projeto com DB usando SQL Alchemy

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ import time
 from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import Form_Criar_Conta, Form_Login
 import email
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '355fcc96736a56eb286df457be1f3597e6e4400d11baed7dccd4f23b76fd0da3262bb29aa85137b88972de87ee11cffca5cd'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
+database = SQLAlchemy(app)
+
 
 @app.route('/')
 def home():
---
+++ models.py
@@ -0,0 +1,20 @@
+from app import database
+from datetime import datetime
+
+class Usuario(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    user_name = database.Column(database.String, nullable=False)
+    email_cadastro = database.Column(database.String, nullable=False, unique=True)
+    senha_cadastro = database.Column(database.String, nullable=False)
+    foto_perfil =  database.Column(database.String, default='default.jpg')
+    posts = database.relationship('Post', backref='Autor', lazy=True)
+    cursor = database.Column(database.String, nullable=False, defautl="Não Informado")
+
+class Post(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    titulo = database.Column(database.Text, nullable=False)
+    corpo = database.Column(database.Text, nullable=False)
+    data_criacao = database.Column(database.DateTime, nullable=False, default = datetime.utcnow())
+    id_usuario = database.Column(database.Integer, database.ForeignKey('usuario.id'), nullable=False)
+
+print(datetime.utcnow())
