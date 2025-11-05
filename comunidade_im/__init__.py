9d94e0820b1f150c68c75c48dd1bc08ada4e0d76
Wede _Borges <wederborgesemp@gmail.com>
2025-11-05 13:28:06 -0400

    Estruturando base de dados do Railway

--- comunidade_im/__init__.py
+++ comunidade_im/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
-
+import sqlalchemy
 app = Flask(__name__)
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
@@ -19,5 +19,18 @@ database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+login_manager.login_message_category = 'alert-info'
+
+
+from comunidade_im import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table('usuario'):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("Base de dados criada com sucesso")
+else:
+    print("Base de dados já existente")
 
 from comunidade_im import routes
